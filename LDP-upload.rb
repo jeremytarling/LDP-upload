da7579194e4a72c5546ed7bbcd5a4031b63f6709
Jeremy Tarling <jeremy.tarling@gmail.com>
2013-03-23 21:02:05 +0000

    first commit

---
+++ LDP-upload.rb
@@ -0,0 +1,61 @@
+require 'restclient'
+require 'json'
+
+# choose an environment form command line args
+env = ARGV[0].to_s
+if env == '--int'
+  HOST = 'https://api.int.bbc.co.uk/'
+elsif env  == '--test'
+  HOST = 'https://api.test.bbc.co.uk/'
+elsif env  ==  '--stage'
+  HOST = 'https://api.stage.bbc.co.uk/'
+elsif env  ==  '--live'
+  HOST = 'https://api.live.bbc.co.uk/'
+elsif env == '--localhost'
+  HOST = 'http://localhost:8080/'
+else puts "usage: must specify environment: --int|--test|--stage|--live|--local"
+end
+
+#assume we;re inside reith
+unless ARGV[1] == '--no-proxy'
+		RestClient.proxy = "http://www-cache.reith.bbc.co.uk/"
+end
+SSL = {
+	:ssl_client_cert => OpenSSL::X509::Certificate.new(File.read("/Users/tarlij01/Documents/certs/Forge.pem")),
+  :ssl_client_key => OpenSSL::PKey::RSA.new(File.read("/Users/tarlij01/Documents/certs/Forge.pem")),
+}
+
+#specify the path and params
+PATH = 'ldp-writer/datasets'
+PARAMS = '?dry-run=true'
+
+#build the URL
+@url = HOST + PATH + PARAMS
+
+#check what we're asking for
+puts "PUTing to #{@url}\n"
+
+# make the request, catch the error if it's not a 200
+begin
+  resource = RestClient::Resource.new(@url, SSL)
+  @response = resource.put(File.read('birmingham-1.ttl'), :content_type => 'application/x-turtle') 
+rescue => e
+  @response = e.response
+end
+
+# show the response code
+puts "=== response code ==="
+puts @response.code
+
+# show the response headers
+#@response_headers = JSON.parse @response.headers
+puts "=== response headers ==="
+puts "date: #{@response.headers[:date]}"
+puts "content type: #{@response.headers[:content_type]}"
+puts "content legth: #{@response.headers[:content_length]}"
+
+# show the response body
+puts "=== response body ==="
+puts @response.body.gsub(/<\/?[^>]*>/, '').gsub(/\n\n+/, "\n").gsub(/^\n|\n$/, '')
+
+
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 LDP-upload
 ==========
-
 ruby script to upload RDF/TTL data to the BBC's Linked Data Platform
+
+doing this through Postman was getting a bit repetitive, ruby + rest-client to the rescue
+
+needs the rest-client gem and a valid BBC SSL cerificate that's been added to the Linked Data Platform's whitelist.
+
+run from the command line:
+
+ruby LDP-upload.rb [--int|--test|--stage|--live] [--no-proxy]
+
+
