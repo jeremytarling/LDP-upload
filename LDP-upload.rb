07a23d8d52b6e6bb7d6141d7b79da96b2ceae7be
Jeremy Tarling <jeremy.tarling@gmail.com>
2013-03-24 13:34:58 +0000

    removed some bloat, added regex to catch returned nextUrl and assign it

--- LDP-upload.rb
+++ LDP-upload.rb
@@ -1,43 +1,22 @@
 require 'restclient'
-require 'json'
 
 # grab the command line arguements
 @ttl = ARGV[0]
-@env = ARGV[1].to_s
-@proxy = ARGV[2].to_s
-
-# choose an environment form command line args
-if @env == '--int'
-  HOST = 'https://api.int.bbc.co.uk/'
-elsif @env  == '--test'
-  HOST = 'https://api.test.bbc.co.uk/'
-elsif @env  ==  '--stage'
-  HOST = 'https://api.stage.bbc.co.uk/'
-elsif @env  ==  '--live'
-  HOST = 'https://api.live.bbc.co.uk/'
-elsif @env == '--localhost'
-  HOST = 'http://localhost:8080/'
-else puts "usage: must specify environment: --int|--test|--stage|--live|--local"
-end
+@proxy = ARGV[1].to_s
 
 #assume we;re inside reith
-unless ARGV[2] == '--no-proxy'
+unless ARGV[1] == '--no-proxy'
   RestClient.proxy = "http://www-cache.reith.bbc.co.uk/"
 end
+
+#set up SSL for RestClient
 SSL = {
   :ssl_client_cert => OpenSSL::X509::Certificate.new(File.read("/Users/tarlij01/Documents/certs/Forge.pem")),
   :ssl_client_key => OpenSSL::PKey::RSA.new(File.read("/Users/tarlij01/Documents/certs/Forge.pem")),
 }
 
-#specify the path and params
-PATH = 'ldp-writer/datasets'
-PARAMS = '?dry-run=true'
-
-#build the URL
-@url = HOST + PATH + PARAMS
-
-#check what we're asking for
-puts "PUTing to #{@url}\n"
+#assign the URL, starting with int and  dry-run
+@url = "https://api.int.bbc.co.uk/ldp-writer/datasets?dry-run=true"
 
 # make the request, catch the error if it's not a 200
 begin
@@ -48,18 +27,15 @@ rescue => e
 end
 
 # show the response code
-puts "=== response code ==="
-puts @response.code
-
-# show the response headers
-#@response_headers = JSON.parse @response.headers
-puts "=== response headers ==="
-puts "date: #{@response.headers[:date]}"
-puts "content type: #{@response.headers[:content_type]}"
-puts "content legth: #{@response.headers[:content_length]}"
+puts "HTTP response code: #{@response.code}"
 
 # show the response body
-puts "=== response body ==="
-puts @response.body.gsub(/<\/?[^>]*>/, '').gsub(/\n\n+/, "\n").gsub(/^\n|\n$/, '')
+puts @response.body
 
+# grab the nextUrl and assign it
+puts "\n=== next URL ==="
+nextUrl = @response.body.lines.grep(/nextUrl/)
+@url = nextUrl[0].to_s.sub("nextUrl=","")
+puts "value of @url now set to #{@url}"
+puts "proceed to next step?"
 
