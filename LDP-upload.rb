080f571cdb53eee59948cce4d187b2643479c0d2
Jeremy Tarling <jeremy.tarling@gmail.com>
2013-03-24 18:17:29 +0000

    re-ordered code into methods

--- LDP-upload.rb
+++ LDP-upload.rb
@@ -1,13 +1,9 @@
 require 'restclient'
 
-# grab the command line arguements
+# grab the cmd line args + set @url to int
 @ttl = ARGV[0]
 @proxy = ARGV[1].to_s
-
-#assume we;re inside reith
-unless ARGV[1] == '--no-proxy'
-  RestClient.proxy = "http://www-cache.reith.bbc.co.uk/"
-end
+@url = "https://api.int.bbc.co.uk/ldp-writer/datasets?dry-run=true"
 
 #set up SSL for RestClient
 SSL = {
@@ -15,27 +11,42 @@ SSL = {
   :ssl_client_key => OpenSSL::PKey::RSA.new(File.read("/Users/tarlij01/Documents/certs/Forge.pem")),
 }
 
-#assign the URL, starting with int and  dry-run
-@url = "https://api.int.bbc.co.uk/ldp-writer/datasets?dry-run=true"
-
-# make the request, catch the error if it's not a 200
-begin
-  resource = RestClient::Resource.new(@url, SSL)
-  @response = resource.put(File.read(@ttl), :content_type => 'application/x-turtle') 
-rescue => e
-  @response = e.response
+#assume we're inside reith
+def getProxy
+  unless ARGV[1] == '--no-proxy'
+    RestClient.proxy = "http://www-cache.reith.bbc.co.uk/"
+  end
 end
 
-# show the response code
-puts "HTTP response code: #{@response.code}"
 
-# show the response body
-puts @response.body
+# make the request, catch the error if it's not a 200
+def putData
+  begin
+    resource = RestClient::Resource.new(@url, SSL)
+    @response = resource.put(File.read(@ttl), :content_type => 'application/x-turtle') 
+  rescue => e
+    @response = e.response
+  end
+end
 
-# grab the nextUrl and assign it
-puts "\n=== next URL ==="
-nextUrl = @response.body.lines.grep(/nextUrl/)
-@url = nextUrl[0].to_s.sub("nextUrl=","")
-puts "value of @url now set to #{@url}"
+def getNextUrl
+  @nextUrl = @response.body.lines.grep(/nextUrl/)
+  @url = @nextUrl[0].to_s.sub("nextUrl=","")
+end
+
+#  talk to me
+def feedBack
+  puts "HTTP response code: #{@response.code}"
+  puts @response.body
+  puts "\n=== next URL ==="
+  getNextUrl
+  puts "value of @url now set to #{@url}"
+end
+
+# call some methods
+getProxy
+putData
+feedBack
+
 puts "proceed to next step?"
-
+gets @nextstep
